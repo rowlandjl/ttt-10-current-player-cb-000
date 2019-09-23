99395310eaa25c74e89e1e187713e36bb54904e1
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-23 14:18:57 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  turn = 0
+
+  board.each do |space|
+    if space == "X" || space == "O"
+      turn += 1
+    else
+      turn += 0
+    end
+  end
+
+  turn
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
